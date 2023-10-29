fb05eb660b5efff5894efadbf697718c8cf8efc4
Saatvik07 <saatvik19097@iiitd.ac.in>
2023-10-30 04:35:22 +0530

    Initialize User resource files

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { OrganisationsModule } from './organisations/organisations.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [PrismaModule, OrganisationsModule],
+  imports: [PrismaModule, OrganisationsModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
