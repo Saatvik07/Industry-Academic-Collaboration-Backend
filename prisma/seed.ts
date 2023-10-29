286b5ea37d05e3d69a0999372b06ca3fa932525c
Saatvik07 <saatvik19097@iiitd.ac.in>
2023-10-29 20:18:48 +0530

    Added swagger for documentation

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -3,6 +3,13 @@ import { PrismaClient, Role } from '@prisma/client';
 const prisma = new PrismaClient();
 
 async function main() {
+  const organizations = await prisma.organization.upsert({
+    where: { name: 'Indraprastha Institute of Information Technology Delhi' },
+    update: {},
+    create: {
+      name: 'Indraprastha Institute of Information Technology Delhi',
+    },
+  });
   const adminUser = await prisma.user.upsert({
     where: { email: 'dummy_admin@sdos.com' },
     update: {},
@@ -12,9 +19,10 @@ async function main() {
       lastName: 'Doe',
       password: 'password123',
       role: Role.ADMIN,
+      orgId: 1,
     },
   });
-  console.log(adminUser);
+  console.log(organizations, adminUser);
 }
 
 // execute the main function
